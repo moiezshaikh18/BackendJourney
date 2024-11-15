6c50efeda44d59e0a617c0998383661690512fae
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-11-15 23:56:00 +0530

    created the maximum and minimum number find out

---
+++ src/Challenges_part2/MaximumAndMinimum_42.java
@@ -0,0 +1,11 @@
+package Challenges_part2;
+
+public class MaximumAndMinimum_42 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int MaximumAndMinimum(int[] array){
+        return  0;
+    }
+}
