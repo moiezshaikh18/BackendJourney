70901f510eb2476ce6b17ff38d3e205f54a5a756
moiezshaikh18 <moiezshaikh18@gmail.com>
2024-11-23 19:04:32 +0530

    Challenges and static and other class

---
+++ src/FiveLectureLoopsArray/Comments.java
@@ -0,0 +1,16 @@
+package FiveLectureLoopsArray;
+
+public class Comments {
+    /**
+     * This is my main method
+     */
+    public static void main(String[] args) {
+        // This is important , don't delete it.
+        System.out.println("Hello World");
+        /*
+        This is first line of comments
+        the comment is still continue
+         */
+
+    }
+}
