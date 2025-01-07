c1532bf3b49fc082651a4ac6ef9dc15fc3cec21d
mueej <v-mueej@fieldera.com>
2025-01-07 16:37:38 +0530

    challenge 106 updated

--- src/challenge106_109/challenge106/TestingLambda.java
+++ src/challenge106_109/challenge106/TestingLambda.java
@@ -1,6 +1,7 @@
 package challenge106_109.challenge106;
 
 import java.util.Comparator;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 
 public class TestingLambda {
@@ -11,5 +12,10 @@ public class TestingLambda {
         int result = multiply.apply(10,5);
 
         System.out.println(STR."Multiplication : \{result}");
+
+        System.out.println("---------------------------------------");
+
+        BiConsumer<Integer,Integer> multiplyNew = (a,b)-> System.out.println(a*b);
+        multiplyNew.accept(4,6);
     }
 }
