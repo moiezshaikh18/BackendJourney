31da8a51312cbeac5dc0a7747ee727c15eea9bb4
mueej <v-mueej@fieldera.com>
2025-01-07 16:32:34 +0530

    challenge 106

---
+++ src/challenge106_109/challenge106/TestingLambda.java
@@ -0,0 +1,15 @@
+package challenge106_109.challenge106;
+
+import java.util.Comparator;
+import java.util.function.BiFunction;
+
+public class TestingLambda {
+    public static void main(String[] args) {
+
+       BiFunction<Integer,Integer,Integer> multiply = (a, b) -> a * b;
+
+        int result = multiply.apply(10,5);
+
+        System.out.println(STR."Multiplication : \{result}");
+    }
+}
